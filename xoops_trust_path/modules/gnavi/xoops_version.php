d342362058d8e16d759b40e9235c0d0b7976fbbf
nao-pon <hypweb@gmail.com>
2013-08-06 07:36:54 +0900

    GNavi version 1.01.2

--- xoops_trust_path/modules/gnavi/xoops_version.php
+++ xoops_trust_path/modules/gnavi/xoops_version.php
@@ -11,7 +11,7 @@ $constpref = '_MI_' . strtoupper( $mydirname ) ;
 
 $modversion['name'] = $mydirname ;
 $modversion['version'] = 1.01 ;
-$modversion['detailed_version'] = '1.01.1' ;
+$modversion['detailed_version'] = '1.01.2' ;
 $modversion['description'] = constant($constpref.'_DESC') ;
 $modversion['credits'] = "Original: GIJOE<br />(http://www.peak.ne.jp/)<br />Daniel Branco<br />(http://bluetopia.homeip.net)<br />Kazumi Ono<br />(http://www.mywebaddons.com/)<br />The XOOPS Project" ;
 $modversion['author'] = "KENTARO (http://xoops.iko-ze.net/)<br />XOOPS X (ten) Distribution Project (https://github.com/XoopsX/gnavi)" ;
